e8167eec93d6a18b8c1744bd78a8c645cce2c734
Kritika Sharma <ishitasharma270604@gmail.com>
2024-01-23 19:59:35 +0530

    using LOF for outlier detection (distance vs distribution based)

--- features/remove_outliers.py
+++ features/remove_outliers.py
@@ -204,11 +204,41 @@ plt.show()
 # Local outlier factor (distance based)
 # --------------------------------------------------------------
 
-# Insert LOF function
+#LOF uses local deviation of the density of a given sample
+# with respect to its neighbors
 
+
+
+
+# Insert LOF function
+def mark_outliers_lof(dataset, columns, n=20):
+    """Mark values as outliers using LOF
+
+    Args:
+        dataset (pd.DataFrame): The dataset
+        col (string): The column you want apply outlier detection to
+        n (int, optional): n_neighbors. Defaults to 20.
+    
+    Returns:
+        pd.DataFrame: The original dataframe with an extra boolean column
+        indicating whether the value is an outlier or not.
+    """
+    
+    dataset = dataset.copy()
+
+    lof = LocalOutlierFactor(n_neighbors=n)
+    data = dataset[columns]
+    outliers = lof.fit_predict(data)
+    X_scores = lof.negative_outlier_factor_
+
+    dataset["outlier_lof"] = outliers == -1
+    return dataset, outliers, X_scores
 
 # Loop over all columns
-
+dataset, outliers , X_scores = mark_outliers_lof(df,outlier_columns)
+for col in outlier_columns:
+   plot_binary_outliers(dataset= dataset, col=col, outlier_col="outlier_lof", reset_index=True)
+plt.show()
 
 # --------------------------------------------------------------
 # Check outliers grouped by label
